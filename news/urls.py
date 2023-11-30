44c40628a6292e102e29beee9b8ec853b1819418
TokarevAlexandr <tokarewalexandr33@gmail.com>
2023-11-30 16:25:23 +0400

    Add files via upload

---
+++ news/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+# Импортируем созданное нами представление
+from .views import (
+    PostsList, PostsDetail, PostsCreate, PostEdit, PostDelete
+)
+
+
+urlpatterns = [
+    # path — означает путь.
+    # В данном случае путь ко всем товарам у нас останется пустым,
+    # чуть позже станет ясно почему.
+    # Т.к. наше объявленное представление является классом,
+    # а Django ожидает функцию, нам надо представить этот класс в виде view.
+    # Для этого вызываем метод as_view.
+    path('', PostsList.as_view(), name='post_list'),
+    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
+    # int — указывает на то, что принимаются только целочисленные значения
+    path('<int:pk>', PostsDetail.as_view(), name='post_detail'),
+    path('create/', PostsCreate.as_view(), name='post_create'),
+    path('<int:pk>/edit/', PostEdit.as_view(), name='post_edit'),
+    path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+]
